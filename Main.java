6e67613e684c73dbad898973dce5164679ab3269
Jan-Lukas Knoch <jl_knoch@protonmail.com>
2017-10-10 22:14:35 +0200

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,87 +1,109 @@
 /** Virtuelles Nim-Spiel
     @author Jan-Lukas Knoch 
-    @version 1.2.1 */
-
+    @version 1.2.6 */
+    
 public class Main
-{
+{  
+    /**
+    Gibt die Spielregeln aus.
+    */
+    public static void zeigeRegeln()
+    {
+        System.out.println("--------------------------------------------------------------------------------------");
+        System.out.println("Regeln: ");
+        System.out.println("Es wird mit 17 Hoelzern gestartet.");
+        System.out.println("Die Spieler ziehen abwechselnd und muessen jeweils mindesten ein Holz ziehen.");
+        System.out.println("Maximal darf die abgerundete Haelfte der insgesamt vorhandenen Hoelzer gezogen werden.");
+        System.out.println("--------------------------------------------------------------------------------------");
+        System.out.println("");
+    }
     
-  public static void zeigeSpielstand (int startwert, int startmaxwert)
-  {
-      String streichholz = "|"; //Zeichen für Hölzer insgesamt
-      String streichholzübrig = "O"; //Zeichen für ziehbare Hölzer
-    
-      //Ausgabe der gesamten Streichhölzer
-      for (int a = 1; a <= startwert; a++) 
-      {
-          System.out.print(streichholz);
-      }
-      System.out.println("");
-        
-      //Ausgabe der übrigen Streichhölzer
-      for (int a = 1; a <= startmaxwert; a++) 
-      {
-          System.out.print(streichholzübrig);
-      }
-      if (startwert == 1 ) 
-      {
-          System.out.print(streichholzübrig);
-      }
-      System.out.println("");
-  }
-    
-  
-  public static void main(String[] args)
-  {
-      // Vordefinieren der Variablen 
-      int startwert = 17; //Hölzer insgesamt
-      int startmaxwert = 8; //Abgerundete Hälfte der insgesamten Hölzer
-    
-      do
-      {
-          int checktoken = 0; //Überprüfungswert
-          int spielerzieht = 0; //Vordefinierte Variable für die Spielereingabe
+    /**
+    Zeigt den aktuellen Spielstand an.
+    @param startwert Insgesamt vorhandene Hoelzer
+    @param startmaxwert Insgesamt ziehbare Hoelzer
+    */
+    public static void zeigeSpielstand(int startwert, int startmaxwert)
+    {
+        String streichholz = "|"; //Zeichen fuer Hoelzer insgesamt
+        String streichholzuebrig = "O"; //Zeichen fuer ziehbare Hoelzer
       
-          do
-          { 
-              zeigeSpielstand(startwert, startmaxwert);
-        
-              //Eingabe des Nutzers 
-              spielerzieht = Input.readInt("Wie viel Hölzer möchtest du ziehen? ");
-        
-              /* Überprüfung der Eingabe **/
-              if (spielerzieht <= startmaxwert) 
-              {
-                  checktoken = 1;
-              } 
-              else 
-              {
-                  if(spielerzieht == startwert) 
-                  {
-                      System.out.println("Du hast gewonnen");
-                      break;
-                  }
-                  System.out.println("Bitte geben Sie einen validen Wert ein");
-              }
-        
-          } 
-          while (checktoken == 0);
+        //Ausgabe der gesamten Streichhoelzer
+        for (int a = 1; a <= startwert; a++) 
+        {
+            System.out.print(streichholz);
+        }
+        System.out.println("");
+          
+        //Ausgabe der uebrigen Streichhoelzer
+        for (int a = 1; a <= startmaxwert; a++) 
+        {
+            System.out.print(streichholzuebrig);
+        }
+        if (startwert == 1 ) 
+        {
+            System.out.print(streichholzuebrig);
+        }
+        System.out.println("");
+    }
       
-          //Berechnen der übrigen Hölzer
-          int verbleibendeHölzer = startwert - spielerzieht;
-          startwert = verbleibendeHölzer;
-          startmaxwert = startwert / 2;
-      
-          //Überprüfen der Ausgabe
-          if(startwert > 0)
-          {
-              System.out.println("Es sind noch " + startwert + " Hölzer vorhanden.");
-          }
-          else
-          {
-              System.out.println("Spiel beendet!");
-          }
-      
-      }
-      while (startwert > 0);
-  }
+    /**
+    Hauptprogramm
+    @param args Liste der Parameter, die beim Programmaufruf angegeben werden.
+    */
+    public static void main(String[] args)
+    {      
+        //Vordefinieren der Variablen 
+        int startwert = 17; //Hoelzer insgesamt
+        int startmaxwert = 8; //Abgerundete Haelfte der insgesamten Hoelzer
+        
+        zeigeRegeln();
+        do
+        {
+            int checktoken = 0; //Ueberpruefungswert
+            int spielerzieht = 0; //Vordefinierte Variable fuer die Spielereingabe
+        
+            do
+            { 
+                zeigeSpielstand(startwert, startmaxwert);
+          
+                //Eingabe des Nutzers 
+                spielerzieht = Input.readInt("Wie viele Hoelzer moechtest du ziehen? ");
+          
+                //Ueberpruefung der Eingabe
+                if (spielerzieht <= startmaxwert) 
+                {
+                    checktoken = 1;
+                } 
+                else 
+                {
+                    if(spielerzieht == startwert) 
+                    {
+                        System.out.println("Du hast gewonnen");
+                        break;
+                    }
+                    System.out.println("Bitte geben Sie einen validen Wert ein");
+                }
+          
+            } 
+            while (checktoken == 0);
+        
+            //Berechnen der uebrigen Hoelzer
+            int verbleibendeHoelzer = startwert - spielerzieht;
+            startwert = verbleibendeHoelzer;
+            startmaxwert = startwert / 2;
+        
+            //Ueberpruefen der Ausgabe
+            if(startwert > 0)
+            {
+                System.out.println("Es sind noch " + startwert + " Hoelzer vorhanden.");
+            }
+            else
+            {
+                System.out.println("Spiel beendet!");
+            }
+        
+        }
+        while (startwert > 0);
+    }
 }
